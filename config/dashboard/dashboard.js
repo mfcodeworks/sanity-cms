1ed859646eb8705e9ddde7336b50168ad813a862
Arran Fletcher <arran.fletcher@linqcash.com>
2020-05-17 16:39:41 +0800

    Added Sanity CMS to Netlify sites for management

--- config/dashboard/dashboard.js
+++ config/dashboard/dashboard.js
@@ -10,6 +10,12 @@ export default {
                         apiId: 'dbe1fb39-4a37-4469-92f6-72131a1afc51',
                         buildHookId: '5ebea926ca4974bf3f355ad5',
                         name: 'blissful-mahavira-71a214'
+                    },
+                    {
+                        title: "Portfolio Sanity CMS",
+                        apiId: "69049695-8e40-4751-88cb-b0b51654f25a",
+                        buildHookId: "5ec0f81ae740ceb8cf1ec617",
+                        name: "amazing-heisenberg-f45ebc"
                     }
                 ]
             }
