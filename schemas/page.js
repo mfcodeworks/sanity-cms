4218d2d45ec8e9f3a1cf850536203bccfee42a7c
Arran Fletcher <arran.fletcher@linqcash.com>
2020-05-17 20:11:00 +0800

    Added page to schema

---
+++ schemas/page.js
@@ -0,0 +1,45 @@
+export default {
+  name: 'page',
+  title: 'Page',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string'
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96
+      }
+    },
+    {
+      name: 'author',
+      title: 'Author',
+      type: 'reference',
+      to: {type: 'author'}
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true
+      }
+    },
+    {
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime'
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent'
+    },
+  ]
+}
