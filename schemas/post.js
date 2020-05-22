14daeb1b49b41d0f7f9e978434ff02d88730e430
Arran Fletcher <arran.fletcher@linqcash.com>
2020-05-22 22:36:03 +0800

    Upgraded Sanity to Canary Test

--- schemas/post.js
+++ schemas/post.js
@@ -63,19 +63,5 @@ export default {
       title: 'Preview iFrame',
       type: 'url'
     }
-  ],
-
-  preview: {
-    select: {
-      title: 'title',
-      author: 'author.name',
-      media: 'mainImage'
-    },
-    prepare(selection) {
-      const {author} = selection
-      return Object.assign({}, selection, {
-        subtitle: author && `by ${author}`
-      })
-    }
-  }
+  ]
 }
